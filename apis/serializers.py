234b37fa6fbafe1aa74b42493c1a07473695033f
Coralie <immacora@gmail.com>
2023-08-18 18:35:27 +0200

    feat(apis): add POST employees with user account endpoint

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,10 +1,100 @@
-from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import get_user_model
+from rest_framework.serializers import ModelSerializer, CharField, ValidationError
 
 from accounts.models import Employee
+
+CustomUser = get_user_model()
+
+
+class CreateCustomUserSerializer(ModelSerializer):
+    """Serializer to create a custom user."""
+
+    password = CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True,
+        required=True,
+        style={"input_type": "password"},
+        validators=[validate_password],
+    )
+    password2 = CharField(
+        max_length=128,
+        min_length=8,
+        write_only=True,
+        required=True,
+        style={"input_type": "password"},
+        validators=[validate_password],
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ("email", "password", "password2")
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise ValidationError({"password": "Password fields didn't match."})
+        attrs.pop("password2")
+        return attrs
+
+
+class CreateEmployeeSerializer(ModelSerializer):
+    """Serializer to create Epic Events employees associated with CustomUser."""
+
+    user = CreateCustomUserSerializer()
+
+    class Meta:
+        model = Employee
+        fields = (
+            "employee_number",
+            "last_name",
+            "first_name",
+            "department",
+            "user",
+            "created_at",
+            "updated_at",
+        )
+
+
+class CustomUserDetailSerializer(ModelSerializer):
+    """Serializer with all custom user informations for creation verification."""
+
+    class Meta:
+        model = CustomUser
+        fields = ("email", "is_active", "is_staff", "date_joined")
+        read_only__fields = "is_staff", "date_joined"
+
+
+class EmployeeDetailSerializer(ModelSerializer):
+    """Serializer with all informations for Epic Events employees detail."""
+
+    user = CustomUserDetailSerializer()
+
+    class Meta:
+        model = Employee
+        fields = (
+            "employee_number",
+            "last_name",
+            "first_name",
+            "department",
+            "user",
+            "created_at",
+            "updated_at",
+        )
+
+
+class CustomUserListSerializer(ModelSerializer):
+    """Serializer with email only for custom user identification."""
+
+    class Meta:
+        model = CustomUser
+        fields = ("email",)
 
 
 class EmployeeListSerializer(ModelSerializer):
     """Serializer with minimal informations for employees list."""
+
+    user = CustomUserListSerializer()
 
     class Meta:
         model = Employee
@@ -12,4 +102,5 @@ class EmployeeListSerializer(ModelSerializer):
             "last_name",
             "first_name",
             "department",
+            "user",
         )
--- apis/views.py
+++ apis/views.py
@@ -1,17 +1,35 @@
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from accounts.models import Employee
 from .serializers import (
     EmployeeListSerializer,
+    CreateEmployeeSerializer,
+    EmployeeDetailSerializer
 )
+
+CustomUser = get_user_model()
 
 
 class EmployeeListAPIView(ListCreateAPIView):
     """
-    Get Epic Events employee list if user IsAuthenticated and is_staff (IsAdminUser).
+    Get Epic Events employee list and create employee account if user IsAuthenticated and is_staff (IsAdminUser).
     """
 
     permission_classes = [IsAuthenticated, IsAdminUser]
     serializer_class = EmployeeListSerializer
     queryset = Employee.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        serializer = CreateEmployeeSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user_data = serializer.validated_data.pop("user")
+            user = CustomUser.objects.create_user(**user_data)
+            employee = Employee.objects.create(user=user, **serializer.validated_data)
+            employee_data = EmployeeDetailSerializer(employee).data
+            return Response(employee_data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
